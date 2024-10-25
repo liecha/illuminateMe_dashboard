724f6127a92c896ab46e3f11269ab891178cfe73
emchju <liecha.jutvik@gmail.com>
2024-10-25 18:57:44 +0200

    Took away the color from barplot

--- streamlit_app.py
+++ streamlit_app.py
@@ -179,13 +179,10 @@ with st.sidebar:
 # Plots
 
 # Barplot
-def make_barplot(input_df, input_y, input_x, input_color, input_color_theme):    
+def make_barplot(input_df, input_y, input_x):    
     barplot = alt.Chart(input_df).mark_bar().encode(
             x=input_x, #'labels',
             y=input_y, #'sportTime(s)'
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
         ) 
     return barplot
 
@@ -334,7 +331,7 @@ with col[0]:
 with col[1]:
     st.markdown('#### Stress factors')
        
-    barplot_sport = make_barplot(df_sport_date, 'sportTime(s)', 'labels', 'time', selected_color_theme)
+    barplot_sport = make_barplot(df_sport_date, 'sportTime(s)', 'labels')
     st.altair_chart(barplot_sport, use_container_width=True)
             
     choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)

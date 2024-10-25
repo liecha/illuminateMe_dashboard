86544d11053ececaecfb3cd2e4538674c9ea8f0a
emchju <liecha.jutvik@gmail.com>
2024-10-25 15:29:33 +0200

    Try if the space is only enough for 2 plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -256,12 +256,9 @@ with col[0]:
 
 with col[1]:
     st.markdown('#### Total Population')
-        
-    choropleth1 = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
-    st.plotly_chart(choropleth1, use_container_width=True)
-    
+            
     choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
     st.plotly_chart(choropleth, use_container_width=True)
     
-    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
+    heatmap = make_barplot()
     st.altair_chart(heatmap, use_container_width=True)

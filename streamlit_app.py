fe2ed9529765544627ddde33922a3e005c895e44
emchju <liecha.jutvik@gmail.com>
2024-10-25 13:51:19 +0200

    Start script

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 13:46:14 2024
+
+@author: emeliechandnijutvik
+"""
+
+import streamlit as st
+st.write('Hello World! :D')

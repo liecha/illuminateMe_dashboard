dcd67ce98dd65021f1ee0598656edcb633ea2c7e
emchju <liecha.jutvik@gmail.com>
2024-10-26 18:51:44 +0200

    Cleaned away text

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,9 @@ def sleep_selection(df_sleep, selected_date):
 # Sidebar
 with st.sidebar:
     st.image("illuminateMe_logo.png")
-    st.markdown('Emelie Chandni Jutvik')    
+    '''
+    st.markdown('Emelie Chandni Jutvik')  
+    '''
     
     # SCORE SELECTION
     # Stress scale:
@@ -221,25 +223,6 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
         ) 
     # height=300
     return heatmap
-
-'''
-# Choropleth map
-def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
-                               color_continuous_scale=input_color_theme,
-                               range_color=(0, max(df_selected_year.population)),
-                               scope="usa",
-                               labels={'population':'Population'}
-                              )
-    choropleth.update_layout(
-        template='plotly_dark',
-        plot_bgcolor='rgba(0, 0, 0, 0)',
-        paper_bgcolor='rgba(0, 0, 0, 0)',
-        margin=dict(l=0, r=0, t=0, b=0),
-        height=350
-    )
-    return choropleth
-'''
 
 # Donut chart
 def make_donut(source):    

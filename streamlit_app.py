0884dedfcd053211688dd57dda65df1f4324d899
emchju <liecha.jutvik@gmail.com>
2024-10-26 14:20:41 +0200

    Trying caption for header sleep time

--- streamlit_app.py
+++ streamlit_app.py
@@ -344,7 +344,7 @@ with col[0]:
                  )
     
     st.markdown('#### Sleep')
-    st.write("You where sleeping for " + sleep_time[0])
+    st.caption("You where sleeping for _:blue[" + sleep_time[0] + "]_")
     categories_sleep = ['deep sleep', 'shallow sleep', 'awake sleep']
     values = df_sleep_date[['deepSleepTime', 'shallowSleepTime', 'wakeTime']].values[0]
     source = pd.DataFrame({

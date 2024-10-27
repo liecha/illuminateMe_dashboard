700df9260022e0a774c4075ae7ac6e744f6ad4c7
emchju <liecha.jutvik@gmail.com>
2024-10-27 07:09:23 +0100

    Trying streamlit_calender basic

--- streamlit_app.py
+++ streamlit_app.py
@@ -319,7 +319,7 @@ with col[0]:
     
     st.markdown('#### Sleep')
     st.caption("You where sleeping for _:blue[" + sleep_time[0] + "]_ at selected date")
-    categories_sleep = ['deep sleep', 'shallow sleep', 'awake sleep']
+    categories_sleep = ['deep sleep', 'shallow sleep', 'awake']
     values = df_sleep_date[['deepSleepTime', 'shallowSleepTime', 'wakeTime']].values[0]
     source = pd.DataFrame({
         "category": categories_sleep,
@@ -342,6 +342,67 @@ with col[1]:
     st.markdown('#### Activity')  
     barplot_sport = make_barplot(df_sport_date, 'labels', 'sportTime(s)')
     st.altair_chart(barplot_sport, use_container_width=True)
+    
+    from streamlit_calendar import calendar
+
+    calendar_options = {
+        "editable": "true",
+        "selectable": "true",
+        "headerToolbar": {
+            "left": "today prev,next",
+            "center": "title",
+            "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+        },
+        "slotMinTime": "06:00:00",
+        "slotMaxTime": "18:00:00",
+        "initialView": "resourceTimelineDay",
+        "resourceGroupField": "building",
+        "resources": [
+            {"id": "a", "building": "Building A", "title": "Building A"},
+            {"id": "b", "building": "Building A", "title": "Building B"},
+            {"id": "c", "building": "Building B", "title": "Building C"},
+            {"id": "d", "building": "Building B", "title": "Building D"},
+            {"id": "e", "building": "Building C", "title": "Building E"},
+            {"id": "f", "building": "Building C", "title": "Building F"},
+        ],
+    }
+    calendar_events = [
+        {
+            "title": "Event 1",
+            "start": "2023-07-31T08:30:00",
+            "end": "2023-07-31T10:30:00",
+            "resourceId": "a",
+        },
+        {
+            "title": "Event 2",
+            "start": "2023-07-31T07:30:00",
+            "end": "2023-07-31T10:30:00",
+            "resourceId": "b",
+        },
+        {
+            "title": "Event 3",
+            "start": "2023-07-31T10:40:00",
+            "end": "2023-07-31T12:30:00",
+            "resourceId": "a",
+        }
+    ]
+    custom_css="""
+        .fc-event-past {
+            opacity: 0.8;
+        }
+        .fc-event-time {
+            font-style: italic;
+        }
+        .fc-event-title {
+            font-weight: 700;
+        }
+        .fc-toolbar-title {
+            font-size: 2rem;
+        }
+    """
+    
+    calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
+    st.write(calendar)
             
     st.markdown('#### Events')  
     barplot_sport = make_barplot(df_sport_date, 'labels', 'sportTime(s)')

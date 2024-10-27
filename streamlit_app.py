f32081eaf607516cb39092af4eff113c911a54a3
emchju <liecha.jutvik@gmail.com>
2024-10-27 07:20:26 +0100

    Moved variables to calendar to top

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,6 +157,66 @@ def sleep_selection(df_sleep, selected_date):
     df_sleep_date = df_sleep_date.div(total_sleep).round(4) * 100
     return df_sleep_date, sleep_time
 
+
+### CALENDAR
+
+calendar_options = {
+    "editable": "true",
+    "selectable": "true",
+    "headerToolbar": {
+        "left": "today prev,next",
+        "center": "title",
+        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
+    },
+    "slotMinTime": "06:00:00",
+    "slotMaxTime": "18:00:00",
+    "initialView": "resourceTimelineDay",
+    "resourceGroupField": "building",
+    "resources": [
+        {"id": "a", "building": "Building A", "title": "Building A"},
+        {"id": "b", "building": "Building A", "title": "Building B"},
+        {"id": "c", "building": "Building B", "title": "Building C"},
+        {"id": "d", "building": "Building B", "title": "Building D"},
+        {"id": "e", "building": "Building C", "title": "Building E"},
+        {"id": "f", "building": "Building C", "title": "Building F"},
+    ],
+}
+
+calendar_events = [
+    {
+        "title": "Event 1",
+        "start": "2023-07-31T08:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "a",
+    },
+    {
+        "title": "Event 2",
+        "start": "2023-07-31T07:30:00",
+        "end": "2023-07-31T10:30:00",
+        "resourceId": "b",
+    },
+    {
+        "title": "Event 3",
+        "start": "2023-07-31T10:40:00",
+        "end": "2023-07-31T12:30:00",
+        "resourceId": "a",
+    }
+]
+custom_css="""
+    .fc-event-past {
+        opacity: 0.8;
+    }
+    .fc-event-time {
+        font-style: italic;
+    }
+    .fc-event-title {
+        font-weight: 700;
+    }
+    .fc-toolbar-title {
+        font-size: 2rem;
+    }
+"""
+
 #######################
 # Sidebar
 with st.sidebar:
@@ -344,65 +404,9 @@ with col[1]:
     barplot_sport = make_barplot(df_sport_date, 'labels', 'sportTime(s)')
     st.altair_chart(barplot_sport, use_container_width=True)
     
-    calendar_options = {
-        "editable": "true",
-        "selectable": "true",
-        "headerToolbar": {
-            "left": "today prev,next",
-            "center": "title",
-            "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
-        },
-        "slotMinTime": "06:00:00",
-        "slotMaxTime": "18:00:00",
-        "initialView": "resourceTimelineDay",
-        "resourceGroupField": "building",
-        "resources": [
-            {"id": "a", "building": "Building A", "title": "Building A"},
-            {"id": "b", "building": "Building A", "title": "Building B"},
-            {"id": "c", "building": "Building B", "title": "Building C"},
-            {"id": "d", "building": "Building B", "title": "Building D"},
-            {"id": "e", "building": "Building C", "title": "Building E"},
-            {"id": "f", "building": "Building C", "title": "Building F"},
-        ],
-    }
-    calendar_events = [
-        {
-            "title": "Event 1",
-            "start": "2023-07-31T08:30:00",
-            "end": "2023-07-31T10:30:00",
-            "resourceId": "a",
-        },
-        {
-            "title": "Event 2",
-            "start": "2023-07-31T07:30:00",
-            "end": "2023-07-31T10:30:00",
-            "resourceId": "b",
-        },
-        {
-            "title": "Event 3",
-            "start": "2023-07-31T10:40:00",
-            "end": "2023-07-31T12:30:00",
-            "resourceId": "a",
-        }
-    ]
-    custom_css="""
-        .fc-event-past {
-            opacity: 0.8;
-        }
-        .fc-event-time {
-            font-style: italic;
-        }
-        .fc-event-title {
-            font-weight: 700;
-        }
-        .fc-toolbar-title {
-            font-size: 2rem;
-        }
-    """
-    
+    st.markdown('#### Events') 
     calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
     st.write(calendar)
-            
-    st.markdown('#### Events')  
+               
     barplot_sport = make_barplot(df_sport_date, 'labels', 'sportTime(s)')
     st.altair_chart(barplot_sport, use_container_width=True)

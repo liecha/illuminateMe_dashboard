e69fca4b1d5f7eee06655adec130ffb79e34bc53
emchju <liecha.jutvik@gmail.com>
2024-10-28 13:26:11 +0100

    See what button press will do

--- streamlit_app.py
+++ streamlit_app.py
@@ -283,6 +283,7 @@ with col[1]:
         selected_peak = st.selectbox('Select a peak', list_of_peaks)
         note = st.text_input("Make a note", "I think this peak refers to...") 
         button_check = st.form_submit_button("Save")
+        print(button_check)
     #st.form_submit_button(label="Save", on_click=save_notes(), type="secondary")
     #st.write("The current movie title is", note)
     

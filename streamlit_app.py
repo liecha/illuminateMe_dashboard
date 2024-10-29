76ae2171e7de3c95829e2952bef55b8a6a6f5c5b
emchju <liecha.jutvik@gmail.com>
2024-10-29 06:45:18 +0100

    Corrected wrong spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -323,7 +323,7 @@ with col[1]:
     
     increment = st.button('Increment')
     if increment:
-        st.session_stat.count += 1
+        st.session_state.count += 1
     
     st.write('Count = ', st.session_state.count)
     

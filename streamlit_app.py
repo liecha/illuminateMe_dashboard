efa41a18b1aa4b3ffc68a22c37e7f44fd9746392
emchju <liecha.jutvik@gmail.com>
2024-10-29 13:45:32 +0100

    Took back button

--- streamlit_app.py
+++ streamlit_app.py
@@ -287,8 +287,8 @@ with col[1]:
         selected_peak = st.selectbox('Select a peak', list_of_peaks)
         placeholder = st.empty()
         input_text = placeholder.text_input('Make your note')
-        #button_check = st.form_submit_button("Save")
-        if len(input_text) != 0:
+        button_check = st.form_submit_button("Save")
+        if input_text:
             input_test = placeholder.text_input('Make your note', value='', key=1)
             st.caption("_Your note was saved_") 
             
